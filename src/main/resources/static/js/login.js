941dbabb1e81aefd72d8863d708c394dcc17a0d8
sww19940620 <616381714@qq.com>
2019-01-17 14:30:35 +0800

    first commit

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,49 @@
+function login()
+{
+    var username = document.getElementById("ID").value;
+    var password = document.getElementById("PASSWORD").value;
+    if(username===""){
+        alert("请输入用户名");
+        return false;
+    }else if(password===""){
+        alert("请输入密码");
+        return false;
+    }
+
+    var params ={};
+    params.userName = username;
+    params.password = password;
+
+    $.ajax({
+        url: "/login/check",
+        type: 'POST',
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify(params),
+        success: function (data) {
+            console.log(data);
+            if(data===0) {
+                sessionStorage.setItem("userName",username);
+
+
+                $.ajax({
+                    url: "/TimeTables/get?userName="+username,
+                    type: 'GET',
+                    success: function (data) {
+                        console.log(data);
+                        sessionStorage.setItem("courese",data);
+                    },
+                    error: function (data) {
+                    }
+                });
+
+                window.location.href='Timetable.html';
+            }
+            else{
+                alert("用户名和密码错误");
+            }
+        },
+        error: function (data) {
+        }
+    });
+}
